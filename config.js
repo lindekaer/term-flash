38f4177022a976d06ae670e0965b6a2200285b31
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2016-10-23 23:30:28 +0200

    Quiz working

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
   // Directory for storing [deck_name].json files
-  deckDir: './decks/',
+  deckDir: './decks',
   defaults: {
     // Default number of questions in a quiz
     numberOfQuestions: 10
